188733dbf6514bd5bcd2df93971d48274bed9685
Vantablackj <datphamnb1@gmail.com>
2024-12-11 19:48:44 +0700

    prrettier and GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-
 import React from 'react';
+import GlobalStyles from '~/components/GlobalStyles';
 
 function App() {
-   
- return (
-    <h1>Hello World!</h1>
- )
+    return <h1>Hello World!</h1>;
 }
 
 export default App;
