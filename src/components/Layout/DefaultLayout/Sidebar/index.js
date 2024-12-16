81228de639a1e8bdf74080392f4254ef3d22ede4
Vantablackj <datphamnb1@gmail.com>
2024-12-16 10:30:53 +0700

    Build a default layout

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Siderbar() {
-    return <h2>Siderbar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Side bar </h2>
+        </aside>
+    );
 }
 
 export default Siderbar;
